f2d62c6ebcd3094e2b49798b7441deab06b8de46
Feynom <chreptak12@gmail.com>
2020-10-28 21:21:50 +0200

    1

---
+++ UnitTest5.1/UnitTest5.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.1/lab5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51
+{
+	TEST_CLASS(UnitTest51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = pryklad(5, 6, 7);
+			Assert::AreEqual(t, 6, 7);
+		}
+	};
+}
